6445dcf167e23c16d0f6901f740788193d426f0d
Tyler Graf <tylergraf@gmail.com>
2019-01-29 10:16:45 -0700

    Add plugin example

---
+++ packages/6-plugin/src/index.js
@@ -0,0 +1,5 @@
+import add from './add.js';
+import subtract from './subtract.js';
+
+// console.log(add(2,3));
+// console.log(subtract(2,3));
---
+++ packages/6-plugin/webpack.config.js
@@ -0,0 +1,13 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new UglifyJsPlugin()
+  ]
+}
